6a94c228beb2717f26b4c7faeb888488c05dec13
thomascswalker <thomascswalker@gmail.com>
2023-01-05 12:46:58 -0500

    convert to clion, update gitignore

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
-#include "json.h"
+#include "src/json.h"
+
+using namespace JSON;
 
 int main()
 {
-    JSON::JsonObject simpleJson = JSON::loadFile("simple.json");
-    std::cout << "Simple JSON:\n" << simpleJson << std::endl << std::endl;
+    std::string filename("..\\examples\\gltf.json");
+    JsonObject jsonFile = loadFile(filename);
+    std::cout << jsonFile << std::endl;
 
-    JSON::JsonObject complexJson = JSON::loadFile("complex.json");
-    std::cout << "Complex JSON:\n" << complexJson << std::endl << std::endl;
-
-    JSON::JsonObject largeJson = JSON::loadFile("large.json");
-    std::cout << "Large JSON:\n" << largeJson << std::endl << std::endl;
-
-    std::string string("{\"fruits\":[\"apple\",\"orange\",\"pear\"]}");
-    JSON::JsonObject stringJson = JSON::loadString(string);
-    std::cout << "Raw String: " << string << std::endl;
-    std::cout << "String JSON:\n" << stringJson << std::endl << std::endl;
-
+    std::string string("{\"test\":5}");
+    JsonObject jsonString = loadString(string);
+    std::cout << jsonString << std::endl;
     return 0;
 }
